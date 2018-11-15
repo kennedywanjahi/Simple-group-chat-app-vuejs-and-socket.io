0ee3851c169f8135fb22b9a896d6dfa095c4007e
ken <kennedywanjahi@gmail.com>
2018-11-15 13:08:17 +0300

    managing connections

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let app = require('express')();
 let http = require('http').Server(app);
 
+let io  = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
+})
+io.on('connection', (socket) => {
+  console.log('a user connected')
+      socket.on('disconnect', ()=>{
+        console.log('user left');
+      } )
+
+})
+
+
 
 http.listen(3000, ()=>{
-  console.log('connected')
-});
+  console.log('we are connected')
+})
